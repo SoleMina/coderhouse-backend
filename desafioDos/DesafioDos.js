c324ae5678bbff9d03a4d63811952853b3297b4b
SoleMina <karina.pradogutierrez@gmail.com>
2021-11-03 15:59:58 -0500

    update

--- desafioDos/DesafioDos.js
+++ desafioDos/DesafioDos.js
@@ -11,16 +11,7 @@ container
   })
   .then((result) => console.log(result.message));
 
-container
-  .save({
-    title: "Cellphone",
-    price: 1200.5,
-    thumbnail:
-      "https://falabella.scene7.com/is/image/FalabellaPE/882255227_1?wid=800&hei=800&qlt=70"
-  })
-  .then((result) => console.log(result.message));
-
-container.getById(1).then((result) => console.log(result));
-container.getAll();
+//container.getById(1).then((result) => console.log(result));
+//container.getAll();
 //container.deleteById(2);
 //container.deleteAll();
