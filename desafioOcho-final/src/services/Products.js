deeaa0ba5f82c34218fb8551287a00a0fc861a90
SoleMina <karina.pradogutierrez@gmail.com>
2021-12-14 17:29:33 -0500

    add products table

--- desafioOcho-final/src/services/Products.js
+++ desafioOcho-final/src/services/Products.js
@@ -7,11 +7,25 @@ export default class Products {
       .hasTable("products")
       .then((result) => {
         if (!result) {
-          database.schema.createTable("products", (table) => {
-            table.increments();
-            table.string("title").notNullable();
-          });
+          database.schema
+            .createTable("products", (table) => {
+              table.increments();
+              table.string("name").notNullable();
+              table.string("description").notNullable();
+              table.float("price").notNullable().defaultTo(false);
+              table.string("thumbnail").notNullable();
+              table.string("codigo").notNullable();
+              table.integer("stock").notNullable().defaultTo(false);
+              table.timestamps(true, true);
+            })
+            .then((result) => {
+              console.log("Products table created");
+            });
         }
       });
   }
+  getProducts = async () => {
+    const products = await database.select().table("products");
+    return { status: "success", payload: products };
+  };
 }
