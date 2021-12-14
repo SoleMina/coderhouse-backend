a2aabb0c4a303029719a05464bc9d9f74e407f60
SoleMina <karina.pradogutierrez@gmail.com>
2021-12-14 16:41:36 -0500

    add database sqlite

--- desafioOcho-final/src/services/Products.js
+++ desafioOcho-final/src/services/Products.js
@@ -1,1 +1,17 @@
-console.log("Hello");
+import database from "../config.js";
+
+export default class Products {
+  constructor() {
+    database
+      .schema()
+      .hasTable("products")
+      .then((result) => {
+        if (!result) {
+          database.schema.createTable("products", (table) => {
+            table.increments();
+            table.string("title").notNullable();
+          });
+        }
+      });
+  }
+}
