e41ee4585f0d6ed7750908e2666da9e71f95847e
SoleMina <karina.pradogutierrez@gmail.com>
2022-01-04 14:08:20 -0500

    update

--- desafioDiez-final/src/config.js
+++ desafioDiez-final/src/config.js
@@ -3,12 +3,5 @@ import __dirname from "./utils.js";
 export default {
   fileSystem: {
     baseUrl: __dirname + "/files/"
-  },
-  mongo: {
-    baseUrl:
-      "mongodb+srv://kprado:Coderhouse123@ecommerce.zw86p.mongodb.net/ecommerce?retryWrites=true&w=majority"
-  },
-  firebase: {
-    baseUrl: "https://ecommerce-9c3f1.firebaseio.com"
   }
 };
