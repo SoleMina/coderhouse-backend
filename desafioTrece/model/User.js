574c72307162385932152950db5b592fbe0231ee
SoleMina <karina.pradogutierrez@gmail.com>
2022-01-27 23:48:18 -0500

    add challenge auth

---
+++ desafioTrece/model/User.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const collection = "Users";
+
+const UserSchema = new mongoose.Schema({
+  first_name: String,
+  last_name: String,
+  username: String,
+  email: String,
+  password: String,
+  age: Number,
+  address: String
+});
+
+export const users = mongoose.model(collection, UserSchema);
---
+++ desafioTrece/passport-config.js
@@ -0,0 +1,41 @@
+import passport from "passport";
+import fbStrategy from "passport-facebook";
+import { users } from "./model/User.js";
+
+const FacebookStrategy = fbStrategy.Strategy;
+
+const initializePassportConfig = () => {
+  passport.use(
+    "facebook",
+    new FacebookStrategy(
+      {
+        clientID: "721247842594027",
+        clientSecret: "a327fb0463fb2ec2c75c065376b01108",
+        callbackURL:
+          "https://b52f-190-236-87-152.ngrok.io/auth/facebook/callback",
+        profileFields: ["emails"]
+      },
+      async (accessToken, refreshToken, profile, done) => {
+        try {
+          console.log(accessToken);
+          console.log(profile);
+          let user = await users.findOne({
+            email: profile.emails[0].value
+          });
+          done(null, user);
+        } catch (error) {
+          done(error);
+        }
+      }
+    )
+  );
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+  passport.deserializeUser(async (id, done) => {
+    let getUser = await user.findById(id);
+    done(id, getUser);
+  });
+};
+
+export default initializePassportConfig;
